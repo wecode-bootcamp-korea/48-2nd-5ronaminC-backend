f4f1e93c9e046b9d3ace1992b22cfc6b96e03858
조승연 <choseungyeon@joseung-yeon-ui-MacBookAir.local>
2023-08-24 16:33:35 +0900

    Add : signUp settings complete

--- api/services/userService.js
+++ api/services/userService.js
@@ -2,6 +2,7 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 const { userDao } = require("../models");
+const { validateSignUp } = require("../utils/validator");
 
 const hashPassword = async (plaintextPassword) => {
   const saltRounds = 10;
@@ -9,7 +10,7 @@ const hashPassword = async (plaintextPassword) => {
   return await bcrypt.hash(plaintextPassword, saltRounds);
 };
 
-const signUp = async (
+const signUp = async(
   username,
   email,
   password,
@@ -18,53 +19,9 @@ const signUp = async (
   gender,
   address,
   postCode,
-  preferredStoreId
+  preferredStoreId,
 ) => {
-const emailRegex = 
-    /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
-const passwordRegex = 
-    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
-const birthdateRegex = 
-    /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/;
-const phoneNumberRegex = 
-    /^[0-9]{11}$/;
-const postCodeRegex = 
-    /^[0-9]{5}$/;
-
-  if (!emailRegex.test(email)) {
-    const error = new Error("INVALID_USER : email");
-    error.statusCode = 400;
-
-    throw error;
-  }
-
-  if (!passwordRegex.test(password)) {
-    const error = new Error("INVALID_USER : password");
-    error.statusCode = 400;
-
-    throw error; 
-  } 
-
-  if (!birthdateRegex.test(birthdate)) {
-    const error = new Error("INVALID_USER : birthdate");
-    error.statusCode = 400;
-
-    throw error; 
-  }
-
-  if (!phoneNumberRegex.test(phoneNumber)) {
-    const error = new Error("INVALID_USER : phone_number"); 
-    error.statusCode = 400;
-    
-    throw error;  
-  } 
-
-  if (!postCodeRegex.test(postCode)) {
-    const error = new Error("INVALID_USER : post_code"); 
-    error.statusCode = 400;
-    
-    throw error;  
-  } 
+  validateSignUp(email, password, birthdate, phoneNumber,postCode);
 
   const hashedPassword = await hashPassword(password);
   const createUser = await userDao.createUser(
@@ -82,7 +39,6 @@ const postCodeRegex =
   return createUser;
 };
 
-
 module.exports = {
-    signUp,
-};
+  signUp,
+}
