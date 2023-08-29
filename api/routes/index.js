4ec04c1bf4b68fd1a17bc7def7ea4fd742d1b373
ghj1 <ghj12222@gmail.com>
2023-08-29 18:04:36 +0900

    장바구니 기능 구현 추가

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,8 +4,11 @@ const routes  = express.Router();
 
 const userRouter = require('./userRouter');
 const wishRouter  = require('./wishRouter');
+const cartRouter = require('./cartRouter');
 
 routes.use('/users', userRouter);
 routes.use('/wishs', wishRouter);
+routes.use('/carts', cartRouter);
+
 
 module.exports = routes;
